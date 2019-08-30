a092d507758f16af8b99b906a5b31c8d36650fea
Aisha <suleamaaisha@yahoo.com>
2019-08-30 14:34:52 +0200

    countdown

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Countdown from './App';
+import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+ReactDOM.render(
+  <Countdown
+timeTillDate="05 26 2019, 6:00 am"
+timeFormat="MM DD YYYY, h:mm a"
+  />, document.getElementById('root'));
